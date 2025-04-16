d17acb75a810696b8677381667153557867bb3ad
apotb <endroo04@gmail.com>
2025-04-16 15:16:48 -0400

    Created the questionnaire

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,7 +8,42 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab1Page {
+  form: any = {
+    moveType: '',
+    destination: '',
+    moveDate: '',
+    hasHousing: false,
+    family: {
+      partner: false,
+      children: false,
+      childrenAges: '',
+      dependents: false,
+      pets: false,
+      petTypes: ''
+    },
+    vehicle: '',
+    currentHousing: '',
+    newHousing: '',
+    services: {
+      internet: false,
+      utilities: false,
+      healthInsurance: false,
+      homeInsurance: false,
+      carInsurance: false
+    },
+    hasJob: false
+  };
 
-  constructor() {}
+  constructor(private toastController: ToastController) {}
 
+  async submitForm() {
+    console.log(this.form);
+    const toast = await this.toastController.create({
+      message: 'Answers saved successfully!',
+      duration: 2000,
+      color: 'success',
+      position: 'middle'
+    });
+    await toast.present();
+  }
 }
