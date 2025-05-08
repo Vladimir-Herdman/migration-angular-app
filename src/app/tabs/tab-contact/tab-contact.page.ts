46721f284e2e9ac316612e8fbf72e568d627798f
AP <endroo04@gmail.com>
2025-05-08 13:47:02 -0400

    Created contact tab, doesn't send email yet

--- src/app/tabs/tab-contact/tab-contact.page.ts
+++ src/app/tabs/tab-contact/tab-contact.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
+import { Auth } from '@angular/fire/auth'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-tab-contact',
@@ -7,10 +11,41 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class TabContactPage implements OnInit {
+  contactForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private toastCtrl: ToastController,
+    private auth: Auth, 
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
+    });
   }
 
+  async submitForm() {
+    if (this.contactForm.valid) {
+      const { name, subject, message } = this.contactForm.value;
+      const email = this.auth.currentUser?.email;
+
+      // Code to send email will go here
+
+      const toast = await this.toastCtrl.create({
+        message: 'Email sent!',
+        duration: 2000,
+        color: 'success',
+        position: 'top'
+      });
+      toast.present();
+
+      this.contactForm.reset();
+
+      this.router.navigateByUrl('/tabs/tab-dashboard', { replaceUrl: false });
+    }
+  }
 }
